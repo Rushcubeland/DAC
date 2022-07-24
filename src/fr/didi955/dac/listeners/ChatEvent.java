211eaedd70e7944cfcad08dd95e079b6c668d22f
Dylan Lannuzel <lannuzeld@gmail.com>
2022-07-24 23:26:43 +0200

    Added quick sanction chat

--- src/fr/didi955/dac/listeners/ChatEvent.java
+++ src/fr/didi955/dac/listeners/ChatEvent.java
@@ -6,16 +6,17 @@ import fr.rushcubeland.commons.AOptions;
 import fr.rushcubeland.commons.Account;
 import fr.rushcubeland.commons.data.callbacks.AsyncCallBack;
 import fr.rushcubeland.commons.options.OptionUnit;
+import fr.rushcubeland.commons.permissions.PermissionsUnit;
 import fr.rushcubeland.commons.rank.RankUnit;
 import fr.rushcubeland.rcbcore.bukkit.RcbAPI;
 import fr.rushcubeland.rcbcore.bukkit.commands.ReportMsgCommand;
 import fr.rushcubeland.rcbcore.bukkit.sanction.MuteData;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -37,12 +38,10 @@ public class ChatEvent implements Listener {
     public void onChat(AsyncPlayerChatEvent event){
         Player player = event.getPlayer();
         String message = event.getMessage();
-        if(MuteData.isInMuteData(player.getUniqueId().toString())){
-            if(!message.startsWith("/")){
-                event.setCancelled(true);
-                player.sendMessage("§cVous avez été mute !");
-                return;
-            }
+        if(MuteData.isInMuteData(player.getUniqueId().toString()) && !message.startsWith("/")){
+            event.setCancelled(true);
+            player.sendMessage("§cVous avez été mute !");
+            return;
         }
         if(ReportMsgCommand.msgs.containsKey(player.getName())){
             ReportMsgCommand.msgs.get(player.getName()).add(message);
@@ -54,39 +53,30 @@ public class ChatEvent implements Listener {
             ReportMsgCommand.msgs.put(player.getName(), msgs);
         }
 
-        RcbAPI.getInstance().getAccount(player, new AsyncCallBack() {
-            @Override
-            public void onQueryComplete(Object result) {
-                Account account = (Account) result;
-                RankUnit rank = account.getRank();
-                TextComponent sign = new TextComponent("⚠");
-                TextComponent format;
-                if (DAC.getInstance().isState(GameState.WAITING) || DAC.getInstance().isState(GameState.STARTING) || DAC.getInstance().isState(GameState.FINISH)) {
-                    format = new TextComponent(rank.getPrefix() + player.getDisplayName() + ": " + message);
-                }
-                else if(rank.getPower() <= 45){
-                    format = new TextComponent(ChatColor.WHITE + player.getDisplayName() + ": " + message);
-                }
-                else
-                {
-                    format = new TextComponent(ChatColor.GRAY + player.getDisplayName() + ": " + message);
-                }
-                sign.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Signaler le message").color(net.md_5.bungee.api.ChatColor.DARK_RED).create()));
-                sign.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/reportmsg " + player.getName() + " " + message));
-                format.setClickEvent(null);
-                format.setHoverEvent(null);
-                for(Player pls : Bukkit.getOnlinePlayers()){
-                    RcbAPI.getInstance().getAccountOptions(pls, new AsyncCallBack() {
-                        @Override
-                        public void onQueryComplete(Object result) {
-                            AOptions aOptions = (AOptions) result;
-                            if(aOptions.getStateChat().equals(OptionUnit.OPEN)){
-                                pls.spigot().sendMessage(new ComponentBuilder(sign).color(net.md_5.bungee.api.ChatColor.DARK_RED).append(format).reset().create());
-                            }
+        RcbAPI.getInstance().getAccount(player, result -> {
+            Account account = (Account) result;
+            TextComponent format = new TextComponent(account.getRank().getPrefix() + player.getDisplayName() + ": " + message);
+            format.setClickEvent(null);
+            format.setHoverEvent(null);
+            for(Player pls : Bukkit.getOnlinePlayers()) {
+                RcbAPI.getInstance().getAccountOptions(pls, result1 -> {
+                    AOptions aOptions = (AOptions) result1;
+                    TextComponent sign;
+                    if (aOptions.getStateChat().equals(OptionUnit.OPEN)) {
+                        if(pls.hasPermission(PermissionsUnit.ALL.getPermission()) || pls.hasPermission(PermissionsUnit.SANCTION_GUI.getPermission()) || pls.hasPermission(PermissionsUnit.SANCTION_GUI_MSG.getPermission())){
+                            sign = new TextComponent("⚒");
+                            sign.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Sanctionner").color(ChatColor.DARK_RED).create()));
+                            sign.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/apmsgb " + player.getName()));
                         }
-                    });
-                }
-                event.setCancelled(true);
+                        else
+                        {
+                            sign = new TextComponent("⚠");
+                            sign.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Signaler le message").color(ChatColor.DARK_RED).create()));
+                            sign.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/reportmsg " + player.getName() + " " + message));
+                        }
+                        pls.spigot().sendMessage(new ComponentBuilder(sign).color(ChatColor.DARK_RED).append(format).reset().create());
+                    }
+                });
             }
         });
         event.setCancelled(true);
